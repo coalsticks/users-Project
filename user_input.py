19d8f3d45d346bcb40a91f360ae128ff9cac54b7
coalsticks <102706146+coalsticks@users.noreply.github.com>
2022-04-27 08:24:02 -0300

    Update user_input.py

--- user_input.py
+++ user_input.py
@@ -1,9 +1,9 @@
-def username_generator() -> str:        # using type-hinting to indicate that the return value is a string
-    name = input("your name: ")         # input function will allow user to write their name
+def username_generator() -> str:        
+    name = input("your name: ")        
 
-    while not name.isalpha():           # while loop is used to iterate the name input in case value
-        name = input("your name: ")     # inserted is numeric instead of alphabetical
-    if len(name) < 5:                   # if the length of the name input is less than 5 characters
+    while not name.isalpha():           
+        name = input("your name: ")    
+    if len(name) < 5:                  
         name + str(len(name))
         return name
     return name[:5] + str(len(name))    # the name is sliced so that only the first 4 digits is present
